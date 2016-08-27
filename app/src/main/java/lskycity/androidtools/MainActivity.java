0b4cbbb10e275af16e58549d8c64f0caa91987a2
lskycity <lskycity@foxmail.com>
2016-08-27 08:24:08 +0800

    show build info

--- app/src/main/java/lskycity/androidtools/MainActivity.java
+++ app/src/main/java/lskycity/androidtools/MainActivity.java
@@ -1,5 +1,6 @@
 package lskycity.androidtools;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +46,46 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText(getSystemInfo());
+    }
+
+    private CharSequence getSystemInfo() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("-------build info -------\n\n");
+        fetchClassInfo(sb, Build.class);
+        sb.append('\n');
+        sb.append("Radio version is ");
+        sb.append(Build.getRadioVersion());
+        sb.append('\n');
+        sb.append('\n');
+        sb.append("-------version info -------\n\n");
+        fetchClassInfo(sb, Build.VERSION.class);
+
+        return sb;
+    }
+
+    private void fetchClassInfo(StringBuilder sb, Class<?> build) {
+        Field[] fields = build.getDeclaredFields();
+        for(Field fd : fields) {
+            int mode = fd.getModifiers();
+            if(Modifier.isPublic(mode) && Modifier.isStatic(mode)) {
+                sb.append(fd.getName());
+                sb.append('=');
+                try {
+                    Object value = fd.get(null);
+                    if(value instanceof Object[]) {
+                        sb.append(Arrays.toString((Object[]) value));
+                    } else {
+                        sb.append(value);
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                sb.append('\n');
+            }
+        }
     }
 
     @Override
