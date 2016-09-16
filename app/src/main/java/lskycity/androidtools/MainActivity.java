c71a58a8b04851cc6a82ae79966a1784fc3537ac
lskycity <lskycity@foxmail.com>
2016-09-16 14:54:45 +0800

    add comment @nonull

--- app/src/main/java/lskycity/androidtools/MainActivity.java
+++ app/src/main/java/lskycity/androidtools/MainActivity.java
@@ -3,6 +3,7 @@ package lskycity.androidtools;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -125,7 +126,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
